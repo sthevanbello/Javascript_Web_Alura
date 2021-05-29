78bec2d1b3f16b2a7edc3cb0a869d3341f4613c9
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-05-28 21:16:48 -0300

    Função para calcular todos os IMCs

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,40 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
+calculaImc(".paciente")
 
-calculaImc("#primeiro-paciente");
-calculaImc("#segundo-paciente");
-calculaImc("#terceiro-paciente");
-calculaImc("#quarto-paciente");
-calculaImc("#quinto-paciente");
-
-
-function calculaImc(pacienteId){
+function calculaImc(pacienteId) {
     var imcVal = true;
-    var paciente = document.querySelector(pacienteId);
+    var pacientes = document.querySelectorAll(pacienteId);
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+    for (let i = 0; i < pacientes.length; i++) {
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+        var paciente = pacientes[i];
 
-    var tdImc = paciente.querySelector(".info-imc");
+        var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
 
-    if (peso <= 0 || peso >= 200)  { 
-        tdPeso.textContent = "Peso inválido";
-        imcVal = false;
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
+
+        var tdImc = paciente.querySelector(".info-imc");
+
+        if (peso <= 0 || peso >= 200) {
+            tdPeso.textContent = "Peso inválido";
+            imcVal = false;
+        }
+        if (altura <= 0 || altura >= 2.50) {
+            tdAltura.textContent = "Altura inválida";
+            imcVal = false;
+        }
+
+        if (imcVal) {
+            var imc = peso / (altura * altura);
+        } else {
+            imc = "Dados inválidos"
+        }
+
+        tdImc.textContent = imc;
     }
-    if (altura <= 0 || altura >= 2.50){
-        tdAltura.textContent = "Altura inválida";
-        imcVal = false;
-    }
-
-    if(imcVal){
-        var imc = peso/(altura * altura);
-    }else{
-        imc = "Dados inválidos"
-    }
-
-    tdImc.textContent = imc;
 }
