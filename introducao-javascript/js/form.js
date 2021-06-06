e6d0e1ecf5f17cf21b4c4c05197587cd5607dbf9
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-06-05 22:59:40 -0300

    Refatoração do código form - Preenchimento dinâmico dos td e do tr

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -7,16 +7,17 @@ function adicionaPaciente(event) {
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
     var tabela = document.querySelector("#tabela-pacientes");
-
     var paciente = recuperaDadosDoForm(form);
     
     var pacienteTr = CriaTr();
     var pacienteTd = CriaTd(paciente);
-
+    
     PreencheTr(pacienteTr, pacienteTd);
-
+    
     preencheTBody(pacienteTr, tabela);
-
+    
+    form.reset()
+    
 }
 
 function CriaTr() {
@@ -25,35 +26,35 @@ function CriaTr() {
     return tr;
 }
 
-function CriaTd(paciente) {
-    
-    var td = {
-        nomeTd: document.createElement("td"),
-        pesoTd: document.createElement("td"),
-        alturaTd: document.createElement("td"),
-        gorduraTd: document.createElement("td"),
-        imcTd: document.createElement("td")
+function CriaTd(dado){
+
+    var tdMontado = {
+        nomeTd: montaTd(dado.nome, "info-nome"),
+        pesoTd: montaTd(dado.peso, "info-peso"),
+        alturaTd: montaTd(dado.altura, "info-altura"),
+        gorduraTd: montaTd(dado.gordura, "info-gordura"),
+        imcTd: montaTd(dado.imc, "info-imc"),
     }
-    td.nomeTd.textContent = paciente.nome;
-    td.pesoTd.textContent = paciente.peso;
-    td.alturaTd.textContent = paciente.altura;
-    td.gorduraTd.textContent = paciente.gordura;
-    td.imcTd.textContent = paciente.imc;
-    
-    return td;
+    return tdMontado
 }
 
-function PreencheTr(pacienteTr, pacienteTd) {
+function montaTd(dado, classe){
 
-    pacienteTr.appendChild(pacienteTd.nomeTd);
-    pacienteTr.appendChild(pacienteTd.pesoTd);
-    pacienteTr.appendChild(pacienteTd.alturaTd);
-    pacienteTr.appendChild(pacienteTd.gorduraTd);
-    pacienteTr.appendChild(pacienteTd.imcTd);
+    var td = document.createElement("td")
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td
 }
 
-function preencheTBody(pacienteTr, tabela) {
-    tabela.appendChild(pacienteTr);
+function PreencheTr(dadoTr, dadoTd) {
+
+    for(let i in dadoTd){
+        dadoTr.appendChild(dadoTd[i]);
+    }
+}
+
+function preencheTBody(dadoTr, tabela) {
+    tabela.appendChild(dadoTr);
 }
 
 function recuperaDadosDoForm(form) {
